adb73ca39e72cd0bf21d58a2341de84eb330e759
Samuel Cozannet <samnco@gmail.com>
2014-11-27 12:32:15 +0100

    first Charm. Still some issues

--- data/rts.storm/src/main/java/com/zdatainc/rts/storm/NodeNotifierBolt.java
+++ data/rts.storm/src/main/java/com/zdatainc/rts/storm/NodeNotifierBolt.java
@@ -1,17 +1,5 @@
 package com.zdatainc.rts.storm;
 
-/**
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;  
-import org.apache.http.impl.client.HttpClients;  
-import org.apache.http.Consts;
-import org.apache.http.HttpEntity;
-**/
 import org.apache.log4j.Logger;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.BasicOutputCollector;
@@ -30,65 +18,36 @@ public class NodeNotifierBolt extends BaseBasicBolt
         Logger.getLogger(NodeNotifierBolt.class);
     private String webserver = Properties.getString("rts.storm.webserv");
     private HttpClient client;
-    /**
-    private CloseableHttpClient httpclient;
-    **/
     private void reconnect()
     {
         this.client = HttpClientBuilder.create().build();
-        /**
-        this.httpclient = HttpClients.createDefault();
-        **/
     }
 
     public void execute(Tuple input, BasicOutputCollector collector)
     {
         Long id = input.getLong(input.fieldIndex("tweet_id"));
-        /**
-        String idStr = Long.toString(id);
-        **/
         String tweet = input.getString(input.fieldIndex("tweet_text"));
-        Float pos = input.getFloat(input.fieldIndex("pos_score"));
-        Float neg = input.getFloat(input.fieldIndex("neg_score"));
-        /**
-        String posStr = Float.toString(pos);
-        String negStr = Float.toString(neg);
-        **/
+        Float pos = input.getFloat(input.fieldIndex("pos_score")) * 100;
+        Float neg = input.getFloat(input.fieldIndex("neg_score")) * 100;
         String score = input.getString(input.fieldIndex("score"));
         HttpPost post = new HttpPost(this.webserver);
         String content = String.format(
-            "{\"id\": \"%d\", "  +
+            "{\"auth_token\": \"YOUR_AUTH_TOKEN\", " +
+            "\"title\": \"%d\", "  +
             "\"text\": \"%s\", " +
-            "\"pos\": \"%f\", "  +
-            "\"neg\": \"%f\", "  +
+            "\"moreinfo\": \"%s\", " +
+            "\"pos\": %f, "  +
+            "\"neg\": %f, "  +
             "\"score\": \"%s\" }",
-            id, tweet, pos, neg, score);
-        /**
-        List<NameValuePair> contentList = new ArrayList<NameValuePair>();
-        contentList.add(new BasicNameValuePair("id", idStr));
-        contentList.add(new BasicNameValuePair("text", tweet));
-        contentList.add(new BasicNameValuePair("pos", posStr));
-        contentList.add(new BasicNameValuePair("pos", negStr));
-        contentList.add(new BasicNameValuePair("score", score));
-        **/
+            id, tweet, tweet, pos, neg, score);
 
         try
         {
             System.out.println("Sent:" + content);
             post.setEntity(new StringEntity(content));
-            /**
-            post.setEntity(new UrlEncodedFormEntity(contentList, Consts.UTF_8));
-            **/
             HttpResponse response = client.execute(post); 
             System.out.println("Got:" + response);
-            /**
-            CloseableHttpResponse response = httpclient.execute(post);
-            **/
             org.apache.http.util.EntityUtils.consume(response.getEntity());
-            /**
-            HttpEntity entity = response.getEntity();
-            response.close();
-            **/
         }
         catch (Exception ex)
         {
@@ -96,9 +55,6 @@ public class NodeNotifierBolt extends BaseBasicBolt
             LOGGER.trace(null, ex);
             reconnect();
         }
-        /** finally {
-            // Closing the response
-        } **/
     }
 
     public void declareOutputFields(OutputFieldsDeclarer declarer) { }
---
+++ hooks/common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Global Variables for the charm
+APPNAME="rts.storm"
+DEST_FOLDER="/opt/storm"
+CONFIG_FILE="${DEST_FOLDER}/${APPNAME}/src/main/resources/rts.storm.properties"
+NODEJS_APP_URI="post"
+JARFILE="target/rts.storm-0.0.1.jar"
